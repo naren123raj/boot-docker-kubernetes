7f9e7b0810fa14672f0bcdc45f71298c5328924e
naren123raj <naren.btech@gmail.com>
2020-11-03 12:48:40 +0800

    First commit

---
+++ src/main/java/com/my/boot/doc/kuber/BootDockerKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.my.boot.doc.kuber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDockerKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootDockerKubernetesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/boot/doc/kuber/BootDockerKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.boot.doc.kuber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootDockerKubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
